5d08976f375af70ff8d71059070744e82c897bb0
bazilix <bazilix@ptotonmail.com>
2019-08-05 03:58:52 +0300

    preparing for location map view and go sleep

---
+++ myPlaces/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  myPlaces
+//
+//  Created by Punchman on 05/08/2019.
+//  Copyright © 2019 bazilixxx. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+}
